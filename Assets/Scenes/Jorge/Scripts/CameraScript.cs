eea7e994be2e804edc9f58b024670f68e9e5081a
guganunes6 <jorge.costa.nunes@tecnico.ulisboa.pt>
2020-10-23 17:05:08 +0100

    Create player movement / camera movement and rotation

---
+++ Assets/Scenes/Jorge/Scripts/CameraScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public float moveSpeed = 10f;
+    public float sensitivity = 10f;
+    public float maxYAngle = 89.5f;
+    private Vector2 currentRotation;
+
+    void Update()
+    {
+
+        ////////// CAMERA & PLAYER POSITIONS
+
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
+
+        player.transform.position = transform.position;
+
+        ////////// CAMERA ROTATION
+
+        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+        Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+
+}
