540165819e94c638c4d6bc1bbaae1b4ae7043d79
guganunes6 <jorge.costa.nunes@tecnico.ulisboa.pt>
2020-10-23 20:40:52 +0100

    Detect collider in front of camera

--- Assets/Scenes/Jorge/Scripts/CameraScript.cs
+++ Assets/Scenes/Jorge/Scripts/CameraScript.cs
@@ -7,7 +7,10 @@ public class CameraScript : MonoBehaviour
 
     public GameObject player;
 
+    public float hitDistance = 2f;
+
     public float moveSpeed = 10f;
+
     public float sensitivity = 10f;
     public float maxYAngle = 89.5f;
     private Vector2 currentRotation;
@@ -32,6 +35,17 @@ public class CameraScript : MonoBehaviour
         Camera.main.transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
         if (Input.GetMouseButtonDown(0))
             Cursor.lockState = CursorLockMode.Locked;
+
+        ////////// RAYCASTS
+
+        RaycastHit hit;
+        var cameraCenter = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, Camera.main.nearClipPlane));
+        if (Physics.Raycast(cameraCenter, transform.forward, out hit, hitDistance))
+        {
+            var obj = hit.transform.gameObject;
+            Debug.Log(obj);
+        }
+
     }
 
 }
