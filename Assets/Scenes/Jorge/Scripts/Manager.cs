6792224f36ceea172451704864a03ebf73af89aa
guganunes6 <jorge.costa.nunes@tecnico.ulisboa.pt>
2020-10-24 22:04:55 +0100

    Create manager and start node feedback

---
+++ Assets/Scenes/Jorge/Scripts/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    private GameObject spheres;
+
+    void Start()
+    {
+        spheres = GameObject.Find("Spheres");
+        int numberSpheres = spheres.transform.childCount;
+        int random = Random.Range(0, numberSpheres);
+        GameObject firstSelectedNode = spheres.transform.GetChild(random).gameObject;
+        NodeFeedback firstTest = firstSelectedNode.AddComponent<NodeFeedback>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Jorge/Scripts/NodeFeedback.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeFeedback : MonoBehaviour
+{
+
+    void Start()
+    {
+        Debug.Log("Started by: " + gameObject);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
