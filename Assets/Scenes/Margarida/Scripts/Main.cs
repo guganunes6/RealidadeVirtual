5c1d710a200b6e424491ae06fa1171fc41abfd89
mag <margarida.lima@tecnico.ulisboa.pt>
2020-11-08 21:07:54 +0000

    - Created class Timer to deal with tasks time counts. \n- Created class CSVEncoder to deal with file management and updating.

--- Assets/Scenes/Margarida/Scripts/Main.cs
+++ Assets/Scenes/Margarida/Scripts/Main.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : MonoBehaviour {
+
+    Timer timer;
     
     void Start() {
-        Decoder decoder = new Decoder("Assets/Scenes/Margarida/Scripts/Dataset/archive/movies_metadata.csv");
-        decoder.Parse(1);
+        //Decoder decoder = new Decoder("Assets/Scenes/Margarida/Scripts/Dataset/archive/movies_metadata.csv");
+        //decoder.Parse(1);
+
+        timer = new Timer();
+        CSVEncoder encoder = new CSVEncoder("tasks_time");
     }
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown("a")) {
+            print("A key was pressed");
+            timer.Start();
+        } else if (Input.GetKeyDown("s")) {
+            print("S key was pressed");
+            timer.Stop();
+            print(timer.GetTime());
+        }
     }
 }
---
+++ Assets/Scenes/Margarida/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class Timer {
+
+    Stopwatch stopwatch;
+
+    public Timer() {
+        stopwatch = new Stopwatch();
+    }
+
+    private void Reset() {
+        stopwatch.Reset();
+    }
+
+    // Resets timer
+    public void StartAgain() {
+        Reset();
+        stopwatch.Start();
+    }
+
+    // Continue 
+    public void Start() {
+        stopwatch.Start();
+    }
+
+    public void Stop() {
+        stopwatch.Stop();
+    }
+
+    public long GetTime() {
+        return stopwatch.ElapsedMilliseconds;
+    }
+
+}
