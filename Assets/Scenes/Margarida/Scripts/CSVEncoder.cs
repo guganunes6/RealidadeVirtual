512d31de4d80192c01d41fdc9b12f822dd453375
mag <margarida.lima@tecnico.ulisboa.pt>
2020-11-08 21:07:54 +0000

    cherry-pick from main: CSVEncoder and Timer

---
+++ Assets/Scenes/Margarida/Scripts/CSVEncoder.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CSVEncoder {
+
+    string fileName;
+    string extension = ".csv";
+    string path = "Assets\\Scenes\\Margarida\\Scripts\\"; 
+
+    long firstTaskTime;
+    long secondTaskTime;
+    long thirdTaskTime;
+
+    public CSVEncoder(string fileName) {
+        this.fileName = fileName;
+        CreateFile(fileName + extension);
+    }
+
+    private void CreateFile(string fileName) {
+        if (FileExists(path + fileName)) {
+            Debug.Log("Creating file");
+            StreamWriter outStream = File.CreateText(path + fileName);
+            outStream.WriteLine("Task 1,Task 2,Task 3");
+            outStream.Close();
+        }
+    }
+
+    // Adds a line to CSV file - It may be a good idea to call this method after SetThirdTaskTime(millis)
+    private void UpdateFile(string fileName) {
+        Debug.Log("Updating file");
+        File.AppendAllText(path + fileName, GetFirstTaskTime() + "," + GetSecondTaskTime() + "," + GetThirdTaskTime() + "\n" );
+    }
+
+    private bool FileExists(string fileName) {
+        return File.Exists(fileName);
+    }
+
+    public void SetFirstTaskTime(long millis) {
+        firstTaskTime = millis;
+    }
+
+    public void SetSecondTaskTime(long millis) {
+        if (firstTaskTime <= 0) { Debug.Log("ALERT: You are inserting the time that the second task takes with the time of the first task equal to 0."); }
+        secondTaskTime = millis;
+    }
+
+    public void SetThirdTaskTime(long millis) {
+        if (firstTaskTime <= 0) { Debug.Log("ALERT: You are inserting the time that the third task takes with the time of the first task equal to 0."); }
+        if (secondTaskTime <= 0) { Debug.Log("ALERT: You are inserting the time that the third task takes with the time of the second task equal to 0."); }
+        thirdTaskTime = millis;
+    }
+
+    public long GetFirstTaskTime() {
+        return firstTaskTime;
+    }
+
+    public long GetSecondTaskTime() {
+        return secondTaskTime;
+    }
+
+    public long GetThirdTaskTime() {
+        return thirdTaskTime;
+    }
+
+    public long GetTotalTasksTime() {
+        return GetFirstTaskTime() + GetSecondTaskTime() + GetThirdTaskTime();
+    }
+
+}
