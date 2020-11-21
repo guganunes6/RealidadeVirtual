58951adbccd952fcc403a05f38e4c584e7c64c4c
guganunes6 <jorge.costa.nunes@tecnico.ulisboa.pt>
2020-11-21 17:24:35 +0000

    Finish Show UI in KBM

--- Assets/Scenes/Diogo/Scripts/MovieUI.cs
+++ Assets/Scenes/Diogo/Scripts/MovieUI.cs
@@ -5,20 +5,37 @@ using UnityEngine.UI;
 using TMPro;
 public class MovieUI : MonoBehaviour
 {
-    public GameObject Title;
+    public GameObject Text;
 
     private void Start()
     {
-
         this.gameObject.SetActive(false);
     }
     public void ShowUI(DecodedNode movie)
     {
+        //title - genres - voteAverage - releaseDate - budget - revenue
         this.gameObject.SetActive(true);
-        Title.GetComponent<TextMeshProUGUI>().SetText("Title: " + movie.getTitle());
+        Text.GetComponent<TextMeshProUGUI>().SetText("<b>Title:</b> " + movie.getTitle() + "\n" + "<b>Genres:</b> " + getStringsFromList(movie.getGenres()) + "\n" + "<b>IMDB:</b> " + movie.getVoteAverage() + "\n" + "<b>Release Date:</b> " + movie.getReleaseDate()/* + "\n" + "Budget: " + movie.getBudget() + "\n" + "Revenue: " + movie.getRevenue()*/);
     }
     public void HideUI()
     {
         this.gameObject.SetActive(false);
+    }
+
+    private string getStringsFromList(List<string> strings)
+    {
+        string decodedString = null;
+        for (int i = 0; i < strings.Count; i++)
+        {
+            if (i == strings.Count - 1)
+            {
+                decodedString += strings[i];
+            }
+            else
+            {
+                decodedString += strings[i] + ", ";
+            }
+        }
+        return decodedString;
     }
 }
