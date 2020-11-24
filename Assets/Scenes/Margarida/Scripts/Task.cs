e01bfc8cac07bc403f7b1c0a4e232254080abb13
FelixMag <margarida.lima@tecnico.ulisboa.pt>
2020-11-24 23:29:53 +0000

    Updated time print

--- Assets/Scenes/Margarida/Scripts/Task.cs
+++ Assets/Scenes/Margarida/Scripts/Task.cs
@@ -24,7 +24,7 @@ public abstract class Task : MonoBehaviour {
     {
         timer.Stop();
         toContinue = true;
-        Debug.Log("Task " + GetTaskId() + " took " + timer.GetTime() + " milisseconds. Press ENTER to continue");
+        Debug.Log("Task " + GetTaskId() + " took " + timer.GetTime() + " seconds. Press ENTER to continue");
     }
 
     public bool ToContinue() {
