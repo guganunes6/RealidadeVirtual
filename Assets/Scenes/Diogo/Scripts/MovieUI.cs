289c1ad8ff0ae0d274ac3db296de6d4c6be82d13
Tiago Neves <45005471+tn7dn7@users.noreply.github.com>
2020-12-09 00:11:37 +0000

    Revert "add animation fadeIn and fadeOut"
    This reverts commit 8afdb48fcf5a846a5713dabd00fa781b736e60bb.

--- Assets/Scenes/Diogo/Scripts/MovieUI.cs
+++ Assets/Scenes/Diogo/Scripts/MovieUI.cs
@@ -6,28 +6,20 @@ using TMPro;
 public class MovieUI : MonoBehaviour
 {
     public GameObject Text;
-    private bool isFadeIn = false;
-    private bool isFadeIn2 = false;
-    private bool isFadeOut = false;
-    private bool isFadeOut2 = false;
 
     private void Start()
     {
         this.gameObject.SetActive(false);
-        this.transform.localScale = new Vector3(0.0f, 0.0f, 1.0f);
     }
     public void ShowUI(DecodedNode movie)
     {
-        isFadeIn = true;
-
         //title - genres - voteAverage - releaseDate - budget - revenue
         this.gameObject.SetActive(true);
         Text.GetComponent<TextMeshProUGUI>().SetText("<b>Title:</b> " + movie.getTitle() + "\n" + "<b>Genres:</b> " + getStringsFromList(movie.getGenres()) + "\n" + "<b>IMDB:</b> " + movie.getVoteAverage() + "\n" + "<b>Release Date:</b> " + movie.getReleaseDate()/* + "\n" + "Budget: " + movie.getBudget() + "\n" + "Revenue: " + movie.getRevenue()*/);
     }
     public void HideUI()
     {
-        isFadeIn = false;
-        isFadeOut = true;
+        this.gameObject.SetActive(false);
     }
 
     private string getStringsFromList(List<string> strings)
@@ -45,55 +37,5 @@ public class MovieUI : MonoBehaviour
             }
         }
         return decodedString;
-    }
-
-    private void animationUI()
-    {
-        if (isFadeIn)
-        {
-            if (this.transform.localScale.x < 1.1f)
-                this.transform.localScale += new Vector3(0.05f, 0.05f, 0.0f);
-            else
-            {
-                isFadeIn = false;
-                isFadeIn2 = true;
-            }
-        }
-
-        if (isFadeIn2)
-        {
-            if (this.transform.localScale.x > 1.0f)
-                this.transform.localScale -= new Vector3(0.005f, 0.005f, 0.0f);
-            else
-                isFadeIn2 = false;
-        }
-
-        if (isFadeOut)
-        {
-            if (this.transform.localScale.x < 1.1f)
-                this.transform.localScale += new Vector3(0.005f, 0.005f, 0.0f);
-            else
-            {
-                isFadeOut = false;
-                isFadeOut2 = true;
-            }
-
-        }
-
-        if (isFadeOut2)
-        {
-            if (this.transform.localScale.x > 0.0f)
-                this.transform.localScale -= new Vector3(0.05f, 0.05f, 0.0f);
-            else
-            {
-                isFadeOut2 = false;
-                this.gameObject.SetActive(false);
-            }
-        }
-    }
-
-    private void Update()
-    {
-        animationUI();
     }
 }
